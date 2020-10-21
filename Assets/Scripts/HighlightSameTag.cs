40ef70543266c685eb28e57960e830014595920c
gilzoide <gilzoide@gmail.com>
2020-10-21 18:47:02 -0300

    Add highlight pras samples na cena Múltiplos Canais

---
+++ Assets/Scripts/HighlightSameTag.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class HighlightSameTag : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public Color defaultColor = Color.white;
+    public Color highlightedColor = Color.yellow;
+    public Graphic[] tintedGraphics;
+
+    public HighlightSameTag[] sameTag;
+
+    void Start()
+    {
+        sameTag = Array.ConvertAll(GameObject.FindGameObjectsWithTag(tag), go => go.GetComponent<HighlightSameTag>());
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        foreach (var sample in sameTag)
+        {
+            sample.Tint(highlightedColor);
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        foreach (var sample in sameTag)
+        {
+            sample.Tint(defaultColor);
+        }
+    }
+
+    void Tint(Color color)
+    {
+        foreach (var graphics in tintedGraphics)
+        {
+            graphics.color = color;
+        }
+    }
+}
