c26882adeb55218885bcd4b3358b185ae33116f6
gilzoide <gilzoide@gmail.com>
2020-10-24 10:13:47 -0300

    Add cena mostrando Audio espacial

---
+++ Assets/Scripts/ButtonOutsideCanvas.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ButtonOutsideCanvas : MonoBehaviour
+{
+    public UnityEvent onClick;
+
+    void OnMouseUpAsButton()
+    {
+        onClick.Invoke();
+    }
+}
